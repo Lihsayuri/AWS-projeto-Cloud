b484321d3082c02075bf4e040ee79a07fb5536c0
Lihsayuri <liviasm1@al.insper.edu.br>
2022-11-13 20:43:50 -0300

    C feito. List, delete e criar funcionando!

--- learn-terraform-aws-instance/data.tf
+++ learn-terraform-aws-instance/data.tf
@@ -14,3 +14,70 @@ data "aws_instances" "instances" {
   # Pegue a tag Name de cada instância
 }
 
+
+
+
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "VisualEditor0",
+#             "Effect": "Allow",
+#             "Action": [
+#                 "ec2:StartInstances",
+#                 "ec2:StopInstances"
+#             ],
+#             "Resource": "arn:aws:ec2:*:116979769772:instance/*"
+#         },
+#         {
+#             "Sid": "VisualEditor1",
+#             "Effect": "Allow",
+#             "Action": "ec2:DescribeInstances",
+#             "Resource": "*"
+#         }
+#     ]
+# }
+
+# START STOP INSTANCES
+
+
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Sid": "VisualEditor0",
+#             "Effect": "Allow",
+#             "Action": "iam:ChangePassword",
+#             "Resource": "arn:aws:iam::116979769772:user/*"
+#         }
+#     ]
+# }
+# CHANGE PASSWORD
+
+
+# GET AND LIST:
+
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Sid": "FirstStatement",
+#       "Effect": "Allow",
+#       "Action": "s3:ListAllMyBuckets",
+#       "Resource": "*"
+#     },
+#     {
+#       "Sid": "SecondStatement",
+#       "Effect": "Allow",
+#       "Action": [
+#         "s3:List*",
+#         "s3:Get*"
+#       ],
+#       "Resource": [
+#         "arn:aws:s3:::confidential-data",
+#         "arn:aws:s3:::confidential-data/*"
+#       ],
+#       "Condition": {"Bool": {"aws:MultiFactorAuthPresent": "true"}}
+#     }
+#   ]
+# }
